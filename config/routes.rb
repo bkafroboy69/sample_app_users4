a1eb333a3ad153a3f493f2fc11d80f44293c9dc5
matthew gamble <matt_gamble07@hotmail.com>
2011-11-29 10:45:54 -0600

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 SampleApp110811::Application.routes.draw do
-  resources :users
 
+
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'signin#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
